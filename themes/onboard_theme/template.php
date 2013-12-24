1b6c0bcebc9e7066204f28842f8698cab14a0925
Geoff Hing <geoffhing@gmail.com>
2013-12-24 02:19:51 -0600

    Basic elements for theme
    * Grid
    * Navbar styles
    * Logo styles
    * Colors
    * Fonts
    * Customize Bootstrap's variables.less
    
    Still to do:
    
    * Create sprites from image assets
    * Footer content. Is it better to hard-code this in the theme, or
    create blocks?
    * Content-type specific pages
    * Favicon
    * Probably a bunch of other stuff
    
    Note that I'm keeping style.css in the repository for now, for
    ease of deployment, but we should just generate this on deployment
    once we figure out a good way to do it.
    
    Addresses #3551

--- themes/onboard_theme/template.php
+++ themes/onboard_theme/template.php
@@ -4,3 +4,39 @@
  * @file
  * template.php
  */
+
+// Number of columns in the grid system
+define('ONBOARD_THEME_GRID_COLUMNS', 16);
+define('ONBOARD_THEME_SIDEBAR_COLUMNS', 4);
+define('ONBOARD_THEME_GRID_CLASS_PREFIX', "col-md-");
+
+/**
+ * Implements hook_preprocess_page().
+ *
+ * @see page.tpl.php
+ */
+function onboard_theme_preprocess_page(&$variables) {
+  // Add information about the number of sidebars.
+  if ((!empty($variables['page']['sidebar_first']) || !empty($variables['page']['logo'])) && !empty($variables['page']['sidebar_second'])) {
+    $content_column_grid_columns = ONBOARD_THEME_GRID_COLUMNS - (ONBOARD_THEME_SIDEBAR_COLUMNS * 2);
+  }
+  elseif (!empty($variables['page']['logo']) || !empty($variables['page']['sidebar_first']) || !empty($variables['page']['sidebar_second'])) {
+    $content_column_grid_columns = ONBOARD_THEME_GRID_COLUMNS - ONBOARD_THEME_SIDEBAR_COLUMNS;
+  }
+  else {
+    $content_column_grid_columns = ONBOARD_THEME_GRID_COLUMNS;
+  }
+  $variables['content_column_class'] = sprintf(' class="%s%d"', ONBOARD_THEME_GRID_CLASS_PREFIX, $content_column_grid_columns);
+  $variables['sidebar_first_class'] = sprintf(' class="%s%d"', ONBOARD_THEME_GRID_CLASS_PREFIX, ONBOARD_THEME_SIDEBAR_COLUMNS); 
+  $variables['sidebar_second_class'] = sprintf(' class="%s%d"', ONBOARD_THEME_GRID_CLASS_PREFIX, ONBOARD_THEME_SIDEBAR_COLUMNS); 
+
+  $variables['navbar_classes_array'] = array('navbar');
+
+  if (theme_get_setting('bootstrap_navbar_inverse')) {
+    $variables['navbar_classes_array'][] = 'navbar-inverse';
+  }
+  else {
+    $variables['navbar_classes_array'][] = 'navbar-default';
+  }
+}
+
